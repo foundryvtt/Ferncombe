4542a81bac7c6e47f965e5f32f7a62689670c09a
Cora <silvative@gmail.com>
2023-05-23 09:19:48 +0100

    Update CSS & remove more leftover AHD code

--- js/ferncombe.mjs
+++ js/ferncombe.mjs
@@ -6,51 +6,13 @@ class FerncombeJournalSheet extends JournalSheet {
   constructor(doc, options) {
     super(doc, options);
     this.options.classes.push("ferncombe", doc.getFlag("ferncombe", "realm"));
-    this.sidebarSections = doc.getFlag("ferncombe", "sidebar-sections") ?? false;
   }
 
   async _renderInner(...args) {
     const html = await super._renderInner(...args);
-    if ( this.sidebarSections ) this._insertSidebarSections(html);
     return html;
   }
 
-  _insertSidebarSections(html) {
-    const toc = html[0].querySelector(".pages-list .directory-list");
-    if ( !toc.children.length ) return;
-    const sections = {locations: false, quests: false, people: false};
-    const divider = document.createElement("li");
-    divider.classList.add("directory-section", "level1");
-    for ( const li of Array.from(toc.children) ) {
-
-      // Locations
-      if ( !sections.locations ) {
-        const d = divider.cloneNode();
-        d.innerHTML = "<h2 class='section-header'>Locations</h2>"
-        li.before(d);
-        sections.locations = true;
-        continue;
-      }
-
-      // People
-      const title = li.querySelector(".page-title").innerText;
-      if ( !sections.people && title.startsWith("Biography:") ) {
-        const d = divider.cloneNode();
-        d.innerHTML = "<h2 class='section-header'>People</h2>"
-        li.before(d);
-        sections.people = true;
-        continue;
-      }
-
-      // Quests
-      if ( !sections.quests && title.startsWith("Quest:") ) {
-        const d = divider.cloneNode();
-        d.innerHTML = "<h2 class='section-header'>Quests</h2>"
-        li.before(d);
-        sections.quests = true;
-      }
-    }
-  }
 }
 
 
